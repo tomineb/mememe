5b25e9d9a48b17aa4822fdf14f37626dff08bdc4
tominetomine <tominetomine@Tomines-MacBook-Air.local>
2024-02-02 23:39:56 +0100

    tweaks

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,49 +4,72 @@ import React, { useState } from "react";
 import Me from "../components/me";
 import Image from "next/image";
 
-const About : React.FC = () =>  {
-    const [selectedContent, setSelectedContent] = useState<string | null>()
-    const showContent = (contentId: string) => {
-        setSelectedContent(contentId);
-    }
+const About: React.FC = () => {
+  const [selectedContent, setSelectedContent] = useState<string | null>();
+  const showContent = (contentId: string) => {
+    setSelectedContent(contentId);
+  };
 
-    return (
+  return (
     <div className="flex flex-row  pt-28">
-    <div className=" w-1/2  pl-28  text-base ">
+      <div className=" w-1/2  pl-28  text-base ">
         <main className="flex flex-col">
-            <a onClick={() => showContent("content3")} className=" hover:text-black cursor-default">Tomine Berge</a>
-            <h1 className=" text-neutral-400"> Software Designer </h1> <br/>
+          <a
+            onClick={() => showContent("content3")}
+            className=" hover:text-black cursor-default"
+          >
+            Tomine Berge
+          </a>
+          <h1 className=" text-neutral-400"> Software Designer </h1> <br />
         </main>
-        <div className="text-neutral-400 flex flex-col">     
-            <span onClick={() => showContent("content1")} className=" hover:text-black cursor-default">About</span>
-            <a href="https://www.linkedin.com/in/tomine-berge/" className=" hover:text-black cursor-default"> Linkedin </a>
-            <a href="https://github.com/tomineb" className=" hover:text-black cursor-default"> Github </a>
-            <a href="" className=" hover:text-black cursor-default"> Medium (?)</a>
+        <div className="text-neutral-400 flex flex-col">
+          <span
+            onClick={() => showContent("content1")}
+            className=" hover:text-black cursor-default"
+          >
+            About
+          </span>
+          <a
+            href="https://www.linkedin.com/in/tomine-berge/"
+            className=" hover:text-black cursor-default"
+          >
+            {" "}
+            Linkedin{" "}
+          </a>
+          <a
+            href="https://github.com/tomineb"
+            className=" hover:text-black cursor-default"
+          >
+            {" "}
+            Github{" "}
+          </a>
+          {/* <a href="" className=" hover:text-black cursor-default"> Medium (?)</a> */}
         </div>
-    </div>
-    <div className=" w-3/4 pl-1/4 ">
+      </div>
+      <div className=" w-3/4 pl-1/4 ">
         {selectedContent ? (
-            <div id={selectedContent} className="m:px-0 flex-row justify-end  sm:pt-10 w-1/2 text-neutral-800 text-sm">
-                {selectedContent === "content1" && (
-                <Me />
-            )}
+          <div
+            id={selectedContent}
+            className="m:px-0 flex-row justify-end  sm:pt-10 w-1/2 text-neutral-800 text-sm"
+          >
+            {selectedContent === "content1" && <Me />}
             {selectedContent === "content3" && (
-                 <div>
-                 <figure>
-                   <Image 
-                     src={`/images/tomine.jpg`}
-                     alt={`Tomine`}
-                     width="240"
-                     height="38"
-                   />
-                 </figure>
-               </div>
+              <div>
+                <figure>
+                  <Image
+                    src={`/images/tomine.jpg`}
+                    alt={`Tomine`}
+                    width="240"
+                    height="38"
+                  />
+                </figure>
+              </div>
             )}
-            </div>
-        ): (
-            <div>
+          </div>
+        ) : (
+          <div>
             <figure>
-              <Image 
+              <Image
                 src={`/images/tomine.jpg`}
                 alt={`Tomine`}
                 width="240"
@@ -55,8 +78,8 @@ const About : React.FC = () =>  {
             </figure>
           </div>
         )}
-    </div>
+      </div>
     </div>
   );
-}
-export default About
+};
+export default About;
--- app/components/clock.tsx
+++ app/components/clock.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Clock from 'react-live-clock';  
+import React from "react";
+import Clock from "react-live-clock";
 
 const ClockWrapper: React.FC = () => {
-    return (
-        <div>
-            <span> TRD </span>
-            <Clock format={'HH:mm:ss'} ticking={true} timezone={'Europe/Oslo'} />
-            </div>
-    )
-}  
-export default ClockWrapper
+  return (
+    <div>
+      <span> TRD </span>
+      <Clock format={"HH:mm:ss"} ticking={true} timezone={"Europe/Oslo"} />
+    </div>
+  );
+};
+export default ClockWrapper;
